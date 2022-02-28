3d88fc9e234a0aa866db86a79e189812e970f21f
PatelSneh21 <patelsneh21@yahoo.com>
2022-02-28 00:54:45 -0600

    Updating backend files, will add login code tmrw once fixed

---
+++ backend/app/__init__.py
@@ -0,0 +1,35 @@
+import os 
+from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, Response
+from flask_cors import CORS
+
+# Import routing, models and Start the App
+from app import views
+
+app = Flask(__name__)
+app.debug = True
+regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
+#db = SQLAlchemy  (app) # flask-sqlalchemy
+
+app.register_blueprint(views.views)	
+
+#The below is an example of how to get environment variables. 
+#API keys, password hash salts, etc will be stored as an environment variable and not uploaded to github.
+#print(os.environ['PASSWORD_SALT'])
+
+def check(email): #Allows us to determine whether or not a given string is a valid email or not 
+    if(re.match(regex, email)):
+        return True 
+    else:
+        return False
+
+#Use this route to test whether or not the backend is running
+@app.route('/status', methods=['GET'])
+def status():
+	return Response(status = 200) 
+
+
+#Application run stuff, in debug mode for now. TURN DEBUG MODE OFF PRIOR TO PRODUCTION, MAKES APP VULNERABLE 
+if __name__ == '__main__':
+	app.debug=True
+	app.run()
--- backend/app/views.py
+++ backend/app/views.py
@@ -1,1 +1,10 @@
 #This is where routes are defined for our application
+# App modules
+from flask import Blueprint
+#from app.models import User
+
+views = Blueprint("views", __name__)
+
+@views.route("/getStatus")
+def getStatus(): 
+	return "Hello"

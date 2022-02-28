a834d6e6b354f0c7735bbdb09a5993750d03daa6
Justin Law <justinlaw801@gmail.com>
2022-02-27 22:07:43 -0600

    sqlalchemy models created using automap

---
+++ backend/app/models.py
@@ -0,0 +1,40 @@
+from re import S
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from numpy import quantile
+from sqlalchemy.ext.automap import automap_base
+
+app = Flask(__name__)
+
+app.config['SQL_DATABASE_URI'] = ""
+
+db = SQLAlchemy(app)
+
+##schema models
+#rack_info = db.Table('rack_info', db.metadata, autoload=True, autoload_with = db.engine)
+
+Base = automap_base()
+Base.prepare(db.engine, reflect=True)
+
+#pulls models by scanning db
+Rack_Info = Base.classes.rack_info
+Bin_Info = Base.classes.bin_info
+Deposits = Base.classes.deposits
+Items = Base.classes.items
+Customers = Base.classes.customers
+Users = Base.classes.users
+Sales = Base.classes.sales
+Samples = Base.classes.samples
+Master_Cartons = Base.classes.master_cartons
+
+
+@app.route('/')
+def index():
+    new_item = Items(item_id="od", item_name="pipe", model_number="123abc", item_price=49.99, quantity )
+    db.session.add(new_item)
+    db.session.commit()
+    #results = db.session.query(Product).all()
+    #for r in results:
+    #    print(r.name)
+    
+    return ""
--- database/schema.sql
+++ database/schema.sql
@@ -17,10 +17,11 @@ USE `warehousedb` ;
 
 -- -----------------------------------------------------
 -- Table `warehousedb`.`rack_info`
+-- stores data on the rack types and locations in the warehouse
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `warehousedb`.`rack_info` ;
+DROP TABLE IF EXISTS `mydb`.`rack_info` ;
 
-CREATE TABLE IF NOT EXISTS `warehousedb`.`rack_info` (
+CREATE TABLE IF NOT EXISTS `mydb`.`rack_info` (
   `rack_id` INT NOT NULL,
   `rack_location` VARCHAR(45) NULL,
   PRIMARY KEY (`rack_id`))
@@ -29,75 +30,82 @@ ENGINE = InnoDB;
 
 -- -----------------------------------------------------
 -- Table `warehousedb`.`bin_info`
+-- data on bin location and size
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `warehousedb`.`bin_info` ;
+DROP TABLE IF EXISTS `mydb`.`bin_info` ;
 
-CREATE TABLE IF NOT EXISTS `warehousedb`.`bin_info` (
+CREATE TABLE IF NOT EXISTS `mydb`.`bin_info` (
   `bin_id` INT NOT NULL,
   `bin_height` INT NULL,
   `bin_location` VARCHAR(45) NULL,
   `rack_info_rack_id` INT NOT NULL,
   PRIMARY KEY (`bin_id`, `rack_info_rack_id`),
-  INDEX `fk_bin_info_rack_info_idx` (`rack_info_rack_id` ASC),
   CONSTRAINT `fk_bin_info_rack_info`
     FOREIGN KEY (`rack_info_rack_id`)
-    REFERENCES `warehousedb`.`rack_info` (`rack_id`)
+    REFERENCES `mydb`.`rack_info` (`rack_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
+CREATE INDEX `fk_bin_info_rack_info_idx` ON `mydb`.`bin_info` (`rack_info_rack_id` ASC);
+
 
 -- -----------------------------------------------------
 -- Table `warehousedb`.`deposits`
+-- data for new incoming shipments
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `warehousedb`.`deposits` ;
+DROP TABLE IF EXISTS `mydb`.`deposits` ;
 
-CREATE TABLE IF NOT EXISTS `warehousedb`.`deposits` (
-  `deposit_id` INT NOT NULL,
+CREATE TABLE IF NOT EXISTS `mydb`.`deposits` (
+  `deposit_id` INT NOT NULL AUTO_INCREMENT,
   `deposit_date` DATETIME NULL,
   `item_id` INT NULL,
   `num_cartons` INT NULL,
   `bin_info_bin_id` INT NOT NULL,
   `bin_info_rack_info_rack_id` INT NOT NULL,
-  PRIMARY KEY (`deposit_id`, `bin_info_bin_id`, `bin_info_rack_info_rack_id`),
-  INDEX `fk_deposits_bin_info1_idx` (`bin_info_bin_id` ASC, `bin_info_rack_info_rack_id` ASC),
+  `items_item_id` INT NOT NULL,
+  PRIMARY KEY (`deposit_id`, `bin_info_bin_id`, `bin_info_rack_info_rack_id`, `items_item_id`),
   CONSTRAINT `fk_deposits_bin_info1`
     FOREIGN KEY (`bin_info_bin_id` , `bin_info_rack_info_rack_id`)
-    REFERENCES `warehousedb`.`bin_info` (`bin_id` , `rack_info_rack_id`)
+    REFERENCES `mydb`.`bin_info` (`bin_id` , `rack_info_rack_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_deposits_items1`
+    FOREIGN KEY (`items_item_id`)
+    REFERENCES `mydb`.`items` (`item_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
+CREATE INDEX `fk_deposits_bin_info1_idx` ON `mydb`.`deposits` (`bin_info_bin_id` ASC, `bin_info_rack_info_rack_id` ASC);
+
+CREATE INDEX `fk_deposits_items1_idx` ON `mydb`.`deposits` (`items_item_id` ASC);
+
 
 -- -----------------------------------------------------
 -- Table `warehousedb`.`items`
+-- data for individual item models
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `warehousedb`.`items` ;
+DROP TABLE IF EXISTS `mydb`.`items` ;
 
-CREATE TABLE IF NOT EXISTS `warehousedb`.`items` (
-  `item_id` INT NOT NULL,
+CREATE TABLE IF NOT EXISTS `mydb`.`items` (
+  `item_id` INT NOT NULL AUTO_INCREMENT,
   `item_name` VARCHAR(45) NULL,
   `model_number` INT NULL,
   `item_price` DECIMAL(10,2) NULL,
   `quantity` INT NULL,
-  `deposits_deposit_id` INT NOT NULL,
-  PRIMARY KEY (`item_id`, `deposits_deposit_id`),
-  INDEX `fk_items_deposits1_idx` (`deposits_deposit_id` ASC),
-  CONSTRAINT `fk_items_deposits1`
-    FOREIGN KEY (`deposits_deposit_id`)
-    REFERENCES `warehousedb`.`deposits` (`deposit_id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
+  PRIMARY KEY (`item_id`))
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
 -- Table `warehousedb`.`customers`
+-- data on customers
 -- -----------------------------------------------------
 DROP TABLE IF EXISTS `warehousedb`.`customers` ;
 
 CREATE TABLE IF NOT EXISTS `warehousedb`.`customers` (
-  `customer_id` INT NOT NULL,
+  `customer_id` INT NOT NULL AUTO_INCREMENT,
   `customer_name` VARCHAR(45) NULL,
   PRIMARY KEY (`customer_id`))
 ENGINE = InnoDB;
@@ -105,11 +113,12 @@ ENGINE = InnoDB;
 
 -- -----------------------------------------------------
 -- Table `warehousedb`.`users`
+-- identifies users with their permissions
 -- -----------------------------------------------------
 DROP TABLE IF EXISTS `warehousedb`.`users` ;
 
 CREATE TABLE IF NOT EXISTS `warehousedb`.`users` (
-  `user_id` INT NOT NULL,
+  `user_id` INT NOT NULL AUTO_INCREMENT,
   `user_name` VARCHAR(45) NULL,
   `user_type` VARCHAR(45) NULL,
   PRIMARY KEY (`user_id`))
@@ -118,11 +127,12 @@ ENGINE = InnoDB;
 
 -- -----------------------------------------------------
 -- Table `warehousedb`.`sales`
+-- data for sales information
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `warehousedb`.`sales` ;
+DROP TABLE IF EXISTS `mydb`.`sales` ;
 
-CREATE TABLE IF NOT EXISTS `warehousedb`.`sales` (
-  `transaction_id` INT NOT NULL,
+CREATE TABLE IF NOT EXISTS `mydb`.`sales` (
+  `transaction_id` INT NOT NULL AUTO_INCREMENT,
   `transaction_date` DATETIME NULL,
   `item_amount` INT NULL,
   `tax_collected` DECIMAL(10,2) NULL,
@@ -133,40 +143,45 @@ CREATE TABLE IF NOT EXISTS `warehousedb`.`sales` (
   `customers_customer_id` INT NOT NULL,
   `users_user_id` INT NOT NULL,
   PRIMARY KEY (`transaction_id`, `items_item_id`, `bin_info_bin_id`, `bin_info_rack_info_rack_id`, `customers_customer_id`, `users_user_id`),
-  INDEX `fk_sales_items1_idx` (`items_item_id` ASC),
-  INDEX `fk_sales_bin_info1_idx` (`bin_info_bin_id` ASC, `bin_info_rack_info_rack_id` ASC),
-  INDEX `fk_sales_customers1_idx` (`customers_customer_id` ASC),
-  INDEX `fk_sales_users1_idx` (`users_user_id` ASC),
   CONSTRAINT `fk_sales_items1`
     FOREIGN KEY (`items_item_id`)
-    REFERENCES `warehousedb`.`items` (`item_id`)
+    REFERENCES `mydb`.`items` (`item_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_sales_bin_info1`
     FOREIGN KEY (`bin_info_bin_id` , `bin_info_rack_info_rack_id`)
-    REFERENCES `warehousedb`.`bin_info` (`bin_id` , `rack_info_rack_id`)
+    REFERENCES `mydb`.`bin_info` (`bin_id` , `rack_info_rack_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_sales_customers1`
     FOREIGN KEY (`customers_customer_id`)
-    REFERENCES `warehousedb`.`customers` (`customer_id`)
+    REFERENCES `mydb`.`customers` (`customer_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_sales_users1`
     FOREIGN KEY (`users_user_id`)
-    REFERENCES `warehousedb`.`users` (`user_id`)
+    REFERENCES `mydb`.`users` (`user_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
+CREATE INDEX `fk_sales_items1_idx` ON `mydb`.`sales` (`items_item_id` ASC);
+
+CREATE INDEX `fk_sales_bin_info1_idx` ON `mydb`.`sales` (`bin_info_bin_id` ASC, `bin_info_rack_info_rack_id` ASC);
+
+CREATE INDEX `fk_sales_customers1_idx` ON `mydb`.`sales` (`customers_customer_id` ASC);
+
+CREATE INDEX `fk_sales_users1_idx` ON `mydb`.`sales` (`users_user_id` ASC);
+
 
 -- -----------------------------------------------------
 -- Table `warehousedb`.`samples`
+-- free samples taken out of the warehouse for marketing
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `warehousedb`.`samples` ;
+DROP TABLE IF EXISTS `mydb`.`samples` ;
 
-CREATE TABLE IF NOT EXISTS `warehousedb`.`samples` (
-  `sample_id` INT NOT NULL,
+CREATE TABLE IF NOT EXISTS `mydb`.`samples` (
+  `sample_id` INT NOT NULL AUTO_INCREMENT,
   `withdraw_date` DATETIME NULL,
   `item_amount` INT NULL,
   `items_item_id` INT NOT NULL,
@@ -174,46 +189,50 @@ CREATE TABLE IF NOT EXISTS `warehousedb`.`samples` (
   `bin_info_rack_info_rack_id` INT NOT NULL,
   `users_user_id` INT NOT NULL,
   PRIMARY KEY (`sample_id`, `items_item_id`, `bin_info_bin_id`, `bin_info_rack_info_rack_id`, `users_user_id`),
-  INDEX `fk_samples_items1_idx` (`items_item_id` ASC),
-  INDEX `fk_samples_bin_info1_idx` (`bin_info_bin_id` ASC, `bin_info_rack_info_rack_id` ASC),
-  INDEX `fk_samples_users1_idx` (`users_user_id` ASC),
   CONSTRAINT `fk_samples_items1`
     FOREIGN KEY (`items_item_id`)
-    REFERENCES `warehousedb`.`items` (`item_id`)
+    REFERENCES `mydb`.`items` (`item_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_samples_bin_info1`
     FOREIGN KEY (`bin_info_bin_id` , `bin_info_rack_info_rack_id`)
-    REFERENCES `warehousedb`.`bin_info` (`bin_id` , `rack_info_rack_id`)
+    REFERENCES `mydb`.`bin_info` (`bin_id` , `rack_info_rack_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_samples_users1`
     FOREIGN KEY (`users_user_id`)
-    REFERENCES `warehousedb`.`users` (`user_id`)
+    REFERENCES `mydb`.`users` (`user_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
+
+CREATE INDEX `fk_samples_items1_idx` ON `mydb`.`samples` (`items_item_id` ASC);
+
+CREATE INDEX `fk_samples_bin_info1_idx` ON `mydb`.`samples` (`bin_info_bin_id` ASC, `bin_info_rack_info_rack_id` ASC);
+
+CREATE INDEX `fk_samples_users1_idx` ON `mydb`.`samples` (`users_user_id` ASC);
 
 
 -- -----------------------------------------------------
 -- Table `warehousedb`.`master_cartons`
+-- information on master cartons for each item
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `warehousedb`.`master_cartons` ;
+DROP TABLE IF EXISTS `mydb`.`master_cartons` ;
 
-CREATE TABLE IF NOT EXISTS `warehousedb`.`master_cartons` (
-  `carton_id` INT NOT NULL,
+CREATE TABLE IF NOT EXISTS `mydb`.`master_cartons` (
+  `carton_id` INT NOT NULL AUTO_INCREMENT,
   `carton_size` VARCHAR(45) NULL,
   `num_items` VARCHAR(45) NULL,
   `items_item_id` INT NOT NULL,
-  `items_deposits_deposit_id` INT NOT NULL,
-  PRIMARY KEY (`carton_id`, `items_item_id`, `items_deposits_deposit_id`),
-  INDEX `fk_master_cartons_items1_idx` (`items_item_id` ASC, `items_deposits_deposit_id` ASC),
+  PRIMARY KEY (`carton_id`, `items_item_id`),
   CONSTRAINT `fk_master_cartons_items1`
-    FOREIGN KEY (`items_item_id` , `items_deposits_deposit_id`)
-    REFERENCES `warehousedb`.`items` (`item_id` , `deposits_deposit_id`)
+    FOREIGN KEY (`items_item_id`)
+    REFERENCES `mydb`.`items` (`item_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
+
+CREATE INDEX `fk_master_cartons_items1_idx` ON `mydb`.`master_cartons` (`items_item_id` ASC);
 
 
 SET SQL_MODE=@OLD_SQL_MODE;

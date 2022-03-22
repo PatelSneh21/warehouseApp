946476f1e7254f466a145e783f497ebcc8ae43d4
root <root@DESKTOP-ILSHRVL.localdomain>
2022-03-21 21:32:53 -0500

    Adding more backend routes

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -10,6 +10,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from numpy import quantile
 from sqlalchemy.ext.automap import automap_base
+from werkzeug.security import generate_password_hash, check_password_hash
 # Import routing, models and Start the App
 from app import views
 
@@ -17,8 +18,10 @@ app = Flask(__name__)
 app.debug = True
 app.permanent_session_lifetime = timedelta(days = 5) #A user who clicks remember me will be logged in for this long
 app.config['SQLALCHEMY_DATABASE_URI'] = "mysql://root:@127.0.0.1:3306/mydb"
+app.config['SECRET_KEY'] = os.environ.get('PASSWORD_SALT')
 
 regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
+salt = os.environ.get('PASSWORD_SALT')
 
 #Database Setup:
 db = SQLAlchemy(app) # flask-sqlalchemy
@@ -30,12 +33,20 @@ Base.prepare(db.engine, reflect=True)
 Rack_Info = Base.classes.rack_info
 Bin_Info = Base.classes.bin_info
 Deposits = Base.classes.deposits
-Items = Base.classes.items
+class Items(db.Model):
+	item_id = db.Column(db.Integer, primary_key = True)
+	item_name = db.Column(db.String(120), unique = True, nullable = False)
+	model_number = db.Column(db.Integer)
+	item_price = db.Column(db.Float)
+	quantity = db.Column(db.Integer)
 Customers = Base.classes.customers
 class Users(db.Model):
 	user_id = db.Column(db.Integer, primary_key = True)
-	user_name = db.Column(db.String(120), unique = True, nullable = False)
+	user_username = db.Column(db.String(120), unique = True, nullable = False)
+	user_name = db.Column(db.String(120), nullable = False)
 	user_type = db.Column(db.String(120), nullable = False)
+	user_password = db.Column(db.String(120), nullable = False)
+
 Sales = Base.classes.sales
 Samples = Base.classes.samples
 Master_Cartons = Base.classes.master_cartons
@@ -63,32 +74,99 @@ def status():
 #Signup a new user
 @app.route('/api/signup', methods =['GET', 'POST'])
 def signup():
-	if "email" in session:
-		return Response(status = 300) #User is already logged in, redirect them to dashboard
 	#Handles being POSTed user data from the form
 	if request.method == 'POST':
-		#email = request.json["email"]
-		#email = email.lower()
+		if "username" in session:
+			return Response(status = 300) #User is already logged in, redirect them to dashboard
+
+		username = request.json["username"]
+		username = username.lower() #Data cleaning
 		name = request.json["name"]
 		userType = request.json["userType"]
-
+		password = request.json["password"]
+		password_hashed = generate_password_hash(password + salt, method = 'sha256')
 		#Input validations to check the data received from the frontend, for the time being generic error 404 is being returned
-		if (name == "" or userType == ""): #No field can be left blank
+		if (name == "" or userType == "" or username == "" or password == ""): #No field can be left blank
 			return Response(status=404)
-		#if not check(email): #The provided email should be a valid email 
-		#	return Response(status=404)
 
-		if (Users.query.filter_by(user_name = name).first() is None):
-			newUser = Users(user_name = name, user_type = userType)
+		if (Users.query.filter_by(user_username = username).first() is None): #Ensures there is no other user with the same username
+			newUser = Users(user_username = username, user_password = password_hashed, user_name = name, user_type = userType)
 			db.session.add(newUser)
-			db.session.commit()	
-			#session["email"] = email  #Log the user in
-			return Response(status = 202)  #Return a status code 202
-		return Response(status = 404) #Email already exists
+			db.session.commit()
 
-	#Our GET Request
+			session["username"] = username 	
+			return Response(status = 202)  #Return a status code 202
+
+		return Response(status = 404) #Username already exists
+
+
+@app.route('/api/login', methods = ['POST', 'GET'])
+def login():
+	if "username" in session:
+		return Response(status = 300) #User is already logged in, redirect to dashboard
+
+	if request.method == 'POST':
+
+		username = request.json["username"]
+		username = username.lower()
+		password = request.json["password"]
+		sessionPermanance = request.json['rememberMe']
+
+		user = Users.query.filter_by(user_username = username).first()
+		if user is None: 
+			return Response(status = 404) #User does not exist
+
+		if check_password_hash(user.user_password, password + salt):
+			session.permanent = sessionPermanance			 		
+			session["username"] = user.user_username
+			return Response(status = 200) #Redirect to dashboard
+
+		return Response(status = 404) #Incorrect Password
+
+	#Our GET Request so that users can access the login page
 	return Response(status = 200)
 
+#Log a user out
+@app.route('/api/logout')
+def logout():
+	if "username" in session is None:
+		return Response(status = 300) #Send user back to homepage if they aren't logged in
+	else: 
+		session.pop("username", None)
+		return Response(status = 200) 
+
+
+@app.route('/api/isLoggedIn', methods = ['GET'])
+def isLoggedIn():
+	user = {}
+	print("Hello1")
+	if ("username" in session):
+
+		current_user = Users.query.filter_by(user_username = session["username"]).first()
+		user = {
+			"username": current_user.user_username,
+			"userType": current_user.user_type
+		}
+		return user, 200
+	else:
+		return Response(status=201)
+
+
+@app.route('/api/addItem', methods = ['POST'])
+def addItem():
+	if "username" in session is None:
+		return Response(status=404)
+
+	else:
+		item_name = request.json["item_name"]
+		model_number = request.json["model_number"]
+		item_price = request.json['item_price']
+		quantity = request.json["quantity"]
+
+		newItem = Items(item_name = item_name, model_number = model_number, item_price = item_price, quantity = quantity)
+		db.session.add(newItem)
+		db.session.commit()
+		return Response(status=200)
 
 #Application run stuff, in debug mode for now. TURN DEBUG MODE OFF PRIOR TO PRODUCTION, MAKES APP VULNERABLE 
 if __name__ == '__main__':
--- backend/app/views.py
+++ backend/app/views.py
@@ -7,65 +7,10 @@ from werkzeug.security import generate_password_hash, check_password_hash
 
 views = Blueprint("views", __name__)
 
-def check(email): #Allows us to determine whether or not a given string is a valid email or not 
-    if(re.match(regex, email)):
-        return True 
-    else:
-        return False
 
 @views.route("/getStatus")
 def getStatus(): 
 	return "Working"
 
-#Login a user
-@views.route('/api/login', methods = ['POST', 'GET'])
-def login():
-	if "email" in session:
-		return Response(status = 300) #User is already logged in, redirect to dashboard
-
-	if request.method == 'POST':
-		email = request.json["email"]
-		email = email.lower()
-		password = request.json["password"]
-		sessionPermanance = request.json['rememberMe']
-
-		user = User.query.filter_by(email = email).first()
-		if user is None: #If there was no user found with that email
-			return Response(status = 404) #Error
-
-		if check_password_hash(user.password_hashed, password + os.environ['PASSWORD_SALT']): #Check if the password matches, if so log them in and send them to the dashboard
-			session.permanent = sessionPermanance	
-			session["email"] = customer.email 
-			return Response(status = 200) #Redirect to dashboard
-
-		return Response(status = 404) #If the password doesn't match then don't log them in and return an error 
-
-	#Our GET Request so that users can access the login page
-	return Response(status = 200)
 
 
-#Log a user out
-@views.route('/api/logout')
-def logout():
-	if "email" in session is None:
-		return Response(status = 300) #Sends the user back to the home page if they are trying to log out without being logged in, redirect to /index
-
-	else: 
-			session.pop("email", None) 
-			return Response(status = 200)
-
-
-#Check if a user is logged in, if so, return the user info
-@views.route('/api/isLoggedIn', methods = ['GET'])
-def isLoggedIn():
-	user = {}
-	if ("email" in session):
-		user = User.query.filter_by(email = session["email"]).first()
-		userInfo = {
-			"firstName": user.firstName,
-			"lastName": user.lastName,
-			"email" : user.email
-		}
-		return user, 202
-	else:
-		return Response(status=200)
